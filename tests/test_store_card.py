c176e14e41180dbc6c66c4a2dcd0ef737dc693b9
wangjohn <wangjohn@mit.edu>
2013-12-29 21:39:42 -0500

    Finishing up the store_card tests.

--- tests/test_store_card.py
+++ tests/test_store_card.py
@@ -20,9 +20,35 @@ class TestStoreCard(zinc_suite.ZincSuite):
             self.run_single(card)
 
     def run_single(self, card):
-        # TODO: send the request
+        payload = self.create_payload(card)
         result = self.post_request(payload)
-        self.verify_response(retailer, result)
+        self.verify_response(result, payload)
 
-    def verify_response(self, retailer, result):
-        #TODO: verify the response
+    def verify_response(self, result, request_payload):
+        nose.tools.assert_equals("store_card_response", result["_type"])
+        nose.tools.assert_is_not_none(result["cc_token"])
+        nose.tools.assert_is_not_none(result["last_four"])
+        nose.tools.assert_is_not_none(result["brand"])
+
+        billing_result = result["billing_address"]
+        billing_payload = request_payload["billing_address"]
+        nose.tools.assert_equals(len(billing_result), len(billing_payload))
+        for key, val in billing_result.iteritems():
+            nose.tools.assert_equals(val, billing_payload[key])
+
+    def create_payload(self, card):
+        return {
+                "number": card[0],
+                "expiration_month": card[1],
+                "expiration_year": card[2],
+                "billing_address": {
+                    "first_name": card[3],
+                    "last_name": card[4],
+                    "address_line1": card[5],
+                    "address_line2": card[6],
+                    "zip_code": card[7],
+                    "city": card[8],
+                    "state": card[9],
+                    "country": card[10]
+                    }
+                }
