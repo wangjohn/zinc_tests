5f0fcddf5f4b874b8df08799aa7996c2b23177f2
wangjohn <wangjohn@mit.edu>
2013-12-30 12:07:10 -0500

    Working on full order integration.

--- tests/test_full_order_integration.py
+++ tests/test_full_order_integration.py
@@ -1,16 +1,33 @@
-import test_store_card
-import test_variant_options
-import test_shipping_methods
+from test_store_card import TestStoreCard
+from test_variant_options import TestVariantOptions
+from test_shipping_methods import TestShippingMethods
 
 class TestFullOrderIntegration:
 
-    def process_data(self):
+    def create_klasses(self):
+        klasses = {}
+        klasses["store_card"] = TestStoreCard()
+        klasses["variant_options"] = TestVariantOptions()
+        klasses["shipping_methods"] = TestShippingMethods()
+
+        for klass in klasses.itervalues():
+            klass.num_tests = 1
+        return klasses
+
+    def process_data(self, klasses):
         data = {}
-        data["store_card"] = test_store_card.TestStoreCard.process_data()
-        data["variant_options"] = test_variant_options.TestVariantOptions.process_data()
-        data["shipping_methods"] = test_shipping_methods.TestShippingMethods.process_data()
+        data["store_card"] = klasses["store_card"].process_data()
+        data["variant_options"] = klasses["variant_options"].process_data()
+        data["shipping_methods"] = klasses["shipping_methods"].process_data()
         return data
 
     def test_full_order_integration(self):
-        test_store_card.TestStoreCard.run_single(
-        pass
+        klasses = self.create_klasses()
+        data = self.process_data(klasses)
+
+        for i in xrange(self.num_tests):
+            klasses["store_card"].run_single(data["store_card"])
+            klasses["variant_options"].run_single(data["variant_options"])
+            klasses["shipping_methods"].run_single(data["shipping_methods"])
+
+
