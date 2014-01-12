1f1d005f3c8c9bbac34422df89d16d782938a535
Max Kolysh <maxko87@gmail.com>
2014-01-12 12:38:41 -0800

    adding support for multiple retailers

--- tests/zinc_suite.py
+++ tests/zinc_suite.py
@@ -15,26 +15,38 @@ def create_logger(filename):
 
 class ZincSuite:
     num_tests = 1
-    zinc_base_url = "https://demotwo.zinc.io/v0"
+    # zinc_base_url = "https://demotwo.zinc.io/v0"
+    zinc_base_url = "http://localhost:5000/v0"
     zinc_url_stub = None
     data_filenames = None
     test_data_path = os.path.join(os.path.dirname(__file__), "../test_data/")
     logger = create_logger("tests.log")
+    retailers = ['amazon']
 
     def read_data(self):
-        if self.data_filenames == None:
-            filenames = [os.path.join(self.test_data_path,
-                self.zinc_url_stub + ".csv")]
-        else:
-            filenames = []
-            for filename_stub in self.data_filenames:
-                filenames.append(os.path.join(self.test_data_path, filename_stub))
+        for retailer in self.retailers:
+            if self.data_filenames == None:
+                retailer_path = os.path.join(self.test_data_path, retailer, self.zinc_url_stub + ".csv")
+                non_retailer_path = os.path.join(self.test_data_path, self.zinc_url_stub + ".csv")
+                print os.path.exists(retailer_path)
+                print os.path.exists(non_retailer_path)
+                if os.path.exists(retailer_path):
+                    filenames = [retailer_path]
+                elif os.path.exists(non_retailer_path):
+                    filenames = [non_retailer_path]
+                else:
+                    filenames = []
 
-        for filename in filenames:
-            with open(filename, "rb") as csvfile:
-                reader = csv.reader(csvfile, delimiter=",")
-                for line in reader:
-                    yield line, filename
+            else:
+                filenames = []
+                for filename_stub in self.data_filenames:
+                    filenames.append(os.path.join(self.test_data_path, filename_stub))
+
+            for filename in filenames:
+                with open(filename, "rb") as csvfile:
+                    reader = csv.reader(csvfile, delimiter=",")
+                    for line in reader:
+                        yield retailer, line, filename
 
     def current_url(self):
         if self.zinc_url_stub:
